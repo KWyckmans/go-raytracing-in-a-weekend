cb1915f4d3f728db856bb7c08ee210c26d98e636
Kevin Wyckmans <kevin@wyckd.be>
2020-03-04 21:23:44 +0100

    generate a sphere and visualise normal maps

--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 
 	"github.com/go-gl/mathgl/mgl32"
 )
 
+func hitSphere(center mgl32.Vec3, radius float32, r Ray) float32 {
+	var oc mgl32.Vec3 = r.Origin().Sub(center)
+
+	var a float32 = r.Direction().Dot(r.Direction())
+	var b float32 = 2 * oc.Dot(r.Direction())
+	var c float32 = oc.Dot(oc) - radius*radius
+
+	var discriminant float32 = b*b - 4*a*c
+
+	if discriminant < 0 {
+		return -1.0
+	} else {
+		return (-b - float32(math.Sqrt(float64(discriminant)))) / (2 * a)
+	}
+}
+
 func color(r Ray) mgl32.Vec3 {
+	var t float32 = hitSphere(mgl32.Vec3{0, 0, -1}, 0.5, r)
+
+	if t > 0 {
+		var N mgl32.Vec3 = r.P(t).Sub(mgl32.Vec3{0, 0, -1}).Normalize()
+		return mgl32.Vec3{N.X() + 1, N.Y() + 1, N.Z() + 1}.Mul(0.5)
+	}
+
 	var unitDirection = r.Direction().Normalize()
-	var t float32 = float32(0.5) * (unitDirection.Y() + 1)
+	t = float32(0.5) * (unitDirection.Y() + 1)
 	return mgl32.Vec3{1, 1, 1}.Mul((1 - t)).Add(mgl32.Vec3{0.5, 0.7, 1.0}.Mul(t))
 }
 
@@ -20,8 +44,8 @@ func check(e error) {
 }
 
 func main() {
-	const nx int = 200
-	const ny int = 100
+	const nx int = 600
+	const ny int = 300
 
 	var lowerLeftCorner = mgl32.Vec3{-2, -1, -1}
 	var horizontal = mgl32.Vec3{4, 0, 0}
@@ -52,9 +76,5 @@ func main() {
 	err := ioutil.WriteFile("dump.ppm", []byte(contents), 0644)
 	check(err)
 
-	var A = mgl32.Vec3{0, 0, 0}
-	var B = mgl32.Vec3{1, 1, 1}
-	var r = Ray{A, B}
-
-	fmt.Println(r.A)
+	fmt.Println("Finished generating image")
 }
